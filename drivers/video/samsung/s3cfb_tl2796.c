27cb6f6a142a2a543365824a52ab3fac1afce6db
Roy Kartadinata <drhonk@gmail.com>
2011-04-27 17:17:21 +0000

    modified FPS cap

--- drivers/video/samsung/s3cfb_tl2796.c
+++ drivers/video/samsung/s3cfb_tl2796.c
@@ -150,7 +150,7 @@ static struct s3cfb_lcd s6e63m0 = {
 	.p_width = 52,
 	.p_height = 86,
 	.bpp = 24,
-	.freq = 60,
+	.freq = 68,
 	
   .timing = {
     .h_fp = 82, 
@@ -925,7 +925,7 @@ static struct s3cfb_lcd s6e63m0 = {
 	.p_width = 52,
 	.p_height = 86,
 	.bpp = 24,
-	.freq = 60,
+	.freq = 68,
 	
 	.timing = {
 		.h_fp = 16,
